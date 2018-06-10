17523ce5c2aaeb22060e2df3b1413dcf086c5701
RAHERIMIHAJA Tokiniaina Nirilanto Venerand <rtnirilantov@gmail.com>
2018-06-10 22:22:44 +0300

    Initial appolo

--- d4u-web/src/index.js
+++ d4u-web/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:4001'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
